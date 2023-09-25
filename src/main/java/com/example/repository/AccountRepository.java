8ab0ed01393de9ab3f6d3b2812e59bf89a462de8
Brandon Ladia <ladia93@gmail.com>
2023-09-25 03:16:19 +0000

    added comments to repository files

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.example.repository;
 
+
+//what table looks like
+// account_id integer primary key auto_increment,
+// username varchar(255) not null unique,
+// password varchar(255)
+
 public interface AccountRepository {
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,11 @@
 package com.example.repository;
 
+//what table looks like
+// message_id integer primary key auto_increment,
+// posted_by integer,
+// message_text varchar(255),
+// time_posted_epoch long,
+// foreign key (posted_by) references Account(account_id)
+
 public interface MessageRepository {
 }
