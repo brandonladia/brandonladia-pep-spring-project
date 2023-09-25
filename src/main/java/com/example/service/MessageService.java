83c1f00b0ad845689169b45a755fc458398dd719
Brandon Ladia <ladia93@gmail.com>
2023-09-25 03:36:02 +0000

    built out constructors and added dependencies

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,9 +1,17 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
 
 @Service
 public class MessageService {
+    MessageRepository messageRepository;
+    @Autowired
+    public MessageService(MessageRepository messageRepository){
+        this.messageRepository = messageRepository;
+    }
 
     //process creation of new message
 
