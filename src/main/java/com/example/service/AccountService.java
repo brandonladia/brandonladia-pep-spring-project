36cef3b6ac346355b59bbac31784c3252ad7fe8e
Brandon Ladia <ladia93@gmail.com>
2023-09-25 03:30:07 +0000

    added structure to services classes

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+
+    //create a user
+
+    //verify login
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -4,4 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+
+    //process creation of new message
+
+    //retrieve all message
+
+    //retrieve message by id
+
+    //delete message by id
+
+    //update message by id
+
+    //retrieve all messages by a particular user
+    
 }
