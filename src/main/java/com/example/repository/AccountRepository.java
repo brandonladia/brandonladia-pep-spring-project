4fa88036fb4378cf801405cf65fbc007e8f72a74
Brandon Ladia <ladia93@gmail.com>
2023-09-26 00:04:08 +0000

    annotations added to repository

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,11 +1,14 @@
 package com.example.repository;
 
+import org.springframework.stereotype.Repository;
 
 //what "account" table looks like
 // account_id integer primary key auto_increment,
 // username varchar(255) not null unique,
 // password varchar(255)
 
+//added Repository
+@Repository
 public interface AccountRepository {
 
     //create a user
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import org.springframework.stereotype.Repository;
+
 //what "message" table looks like
 // message_id integer primary key auto_increment,
 // posted_by integer,
@@ -7,8 +9,10 @@ package com.example.repository;
 // time_posted_epoch long,
 // foreign key (posted_by) references Account(account_id)
 
+@Repository
 public interface MessageRepository {
 
+    //maybe?
     //process creation of new message
 
     //retrieve all message
